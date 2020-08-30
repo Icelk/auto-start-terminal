a30fed1275cda2ba5d8afd2c308c6c3911e1649e
Iselk <62830417+Iselk@users.noreply.github.com>
2020-08-30 16:43:37 +0200

    Added prepared commands

--- src/extension.ts
+++ src/extension.ts
@@ -5,28 +5,32 @@ export function activate (/* context: vscode.ExtensionContext */)
 	const settings = vscode.workspace.getConfiguration();
 
 	const startCommands = settings.get("auto-start-terminal.commands") as string[];
+	const startHelps = settings.get("auto-start-terminal.preparedCommands") as string[];
+
 
 	if (startCommands === undefined || startCommands.length === 0)
 	{
 		return;
 	}
 
+	const commands = startCommands.map((value) => { return { command: value, start: true }; }).concat(...startHelps.map((value) => { return { command: value, start: false }; }));
+
 	let terminals = Array<vscode.Terminal>();
 
 	const overrideActive = settings.get("auto-start-terminal.overrideActive") as boolean;
 
-	for (let i = 0; i < startCommands.length; i++)
+	for (let i = 0; i < commands.length; i++)
 	{
 		if (i === 0 && vscode.window.activeTerminal !== undefined && overrideActive)
 		{
 			let terminal = vscode.window.activeTerminal;
-			terminal.sendText(startCommands[i], true);
+			terminal.sendText(commands[i].command, commands[i].start);
 			terminals.push(terminal);
 		}
 		else
 		{
 			let terminal = vscode.window.createTerminal();
-			terminal.sendText(startCommands[i], true);
+			terminal.sendText(commands[i].command, commands[i].start);
 			terminals.push(terminal);
 		}
 	}
